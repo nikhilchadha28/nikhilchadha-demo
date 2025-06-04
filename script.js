05ccad0ccf317d7bbe6f1017fd9866fece212911
Nikhil Chadha <nikhilchadha828@gmail.com>
2025-06-04 19:01:22 +0800

    Add basic clothing shop web app

---
+++ script.js
@@ -0,0 +1,17 @@
+const products = [
+    { name: 'T-Shirt', price: '$20' },
+    { name: 'Jeans', price: '$40' },
+    { name: 'Jacket', price: '$60' }
+];
+
+function renderProducts() {
+    const container = document.querySelector('.product-list');
+    products.forEach(p => {
+        const div = document.createElement('div');
+        div.className = 'product';
+        div.innerHTML = `<h3>${p.name}</h3><p class="price">${p.price}</p>`;
+        container.appendChild(div);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', renderProducts);
